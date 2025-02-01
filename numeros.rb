6a992029c6d321c3127395b8ca6ad0d934180681
Mario Azevedo <mariobct@msn.com>
2025-02-01 12:55:39 -0300

    numeros.rb

---
+++ numeros.rb
@@ -0,0 +1,14 @@
+puts "Insira um número: "
+x = gets.chomp.to_i
+puts "Insira outro número: "
+y = gets.chomp.to_i
+
+soma = x + y
+subt = x - y
+mult = x * y
+div = x / y
+
+puts "A soma desses números é #{soma}"
+puts "A subtração desses números é #{subt}"
+puts "O produto desses números é #{mult}"
+puts "A divisão desses números é #{div}"
