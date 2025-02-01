e178953915898ac79c6311365e67b8b5170ca64a
Mario Azevedo <mariobct@msn.com>
2025-02-01 20:02:48 -0300

    Condicionais/if, elsif e else

---
+++ if.rb
@@ -0,0 +1,19 @@
+x = 10
+
+if x == 11
+  puts "x é igual a 10"
+elsif x == 50
+  puts "X é igual a 50"
+else
+  puts "x não é igual a 10"
+end
+
+nome = "Mario"
+
+if nome == "Mario"
+  puts "Que nome bonito!"
+  elsif nome == "João" 
+    puts "Nome normal"
+  else
+    puts "Nome estranho"
+end
