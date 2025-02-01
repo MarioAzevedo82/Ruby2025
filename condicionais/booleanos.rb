c5dc64f5c34bc4bbe1b12cec73d057ea095fa548
Mario Azevedo <mariobct@msn.com>
2025-02-01 19:52:52 -0300

    Condicionais/booleanos

---
+++ condicionais/booleanos.rb
@@ -0,0 +1,22 @@
+# ...
+# AND
+# V e V = V
+# V e F = F
+# F e V = F
+# F e F = F 
+
+# OR 
+# V ou V = V
+# V ou F = V
+# F ou V = V
+# F ou F = F
+
+# NOT
+# Não Verdadeiro = Falso
+# Não Falso = Vardadeiro
+# ...
+
+puts false && true
+puts false || true
+puts !false && true
+
