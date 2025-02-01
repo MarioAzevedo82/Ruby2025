4103b1f1448248c9dbc94fca5c2feb8a9825138a
Mario Azevedo <mariobct@msn.com>
2025-02-01 12:37:32 -0300

    Hello.rb

---
+++ hello.rb
@@ -0,0 +1,11 @@
+# STRINGS E VARIÁVEIS
+puts "Digite seu nome:"
+nome = gets.chomp
+
+puts "Digite a sua idade: "
+idade = gets.chomp.to_i
+
+
+puts "hello #{nome}"
+puts "#{nome} tem #{idade} anos"
+
