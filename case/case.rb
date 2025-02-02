3410648943f6141867ba44e493a3e489a553a701
Mario Azevedo <mariobct@msn.com>
2025-02-02 13:01:17 -0300

    Case

---
+++ case/case.rb
@@ -0,0 +1,13 @@
+mes = "janeiro"
+
+case mes
+when "janeiro"
+  puts "tem 31 dias"
+when "fevereiro"
+  puts "tem 28 dias"
+when "março"
+  puts "tem 31 dias"
+else
+  puts "não sei qnts dias tem esse mês"
+end
+ 
